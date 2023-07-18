11e1fa2e031a9871beebd5d71378498711108d1d
Burak Sag <iburaksag@gmail.com>
2023-07-18 20:28:12 +0300

    Basic structures of routes is done.

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,27 @@
 const getAllTasks = (req, res) => {
-    res.send('all items FROM the file')
+    res.send('Get all tasks')
+}
+
+const createTask = (req, res) => {
+    res.send('Create task')
+}
+
+const getTask = (req, res) => {
+    res.send('Get Single task')
+}
+
+const updateTask = (req, res) => {
+    res.send('update task')
+}
+
+const deleteTask = (req, res) => {
+    res.send('delete task')
 }
 
 module.exports = {
     getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask,
 }
