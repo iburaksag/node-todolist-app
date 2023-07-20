ac9dee8095e19cc493e090936c96f0b9f91de218
Burak Sag <iburaksag@gmail.com>
2023-07-20 15:13:11 +0300

    Mongoose imported. DB Connection is provided

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const { getAllTasks, createTask, getTask, updateTask, deleteTask } = require('../controllers/tasks')
 
 router.route('/').get(getAllTasks).post(createTask)
-router.route('/:id').get(getTask).patch(updateTask).deleteTask(deleteTask)
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
 
 module.exports = router
